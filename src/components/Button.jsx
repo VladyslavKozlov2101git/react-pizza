c32fe687979fe82fabd76135357992b3f644bc16
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-10-25 16:51:05 +0300

    add select categories lesson 3

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classNames from 'classnames'
+
+function Button({ className, outline, onClick, children }) {
+    return (
+        <button
+            onClick={onClick}
+            className={classNames('button', className, {
+                'button--outline': outline,
+            })}>
+
+            {children}
+
+        </button>
+    )
+}
+
+export default Button
