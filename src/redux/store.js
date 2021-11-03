2d5845b855b52b442be06cd56aa53a846541f4e6
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-11-03 21:47:33 +0200

    Lesson 6 1h49m

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
-import {createStore} from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-const store = createStore()
+import rootReducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
